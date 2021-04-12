1a8fef7f99ca7376ae9dbf568187540b24228897
player-zero <general.dmf@gmail.com>
2021-04-12 22:08:43 +0300

    interface and repository added

---
+++ library/models/BookRepository.ts
@@ -0,0 +1,11 @@
+export class BookRepository {
+  async getBook(id: string) {}
+
+  async getBooks() {}
+
+  async createBook(book: iBook) {}
+
+  async updateBook(id: string) {}
+
+  async deleteBook(id: string) {}
+}
---
+++ library/models/iBook.interface.ts
@@ -0,0 +1,9 @@
+interface iBook {
+    title: string;
+    description: string;
+    authors: string;
+    favorite: string;
+    fileCover: string;
+    fileName: string;
+    fileBook: string;
+}
